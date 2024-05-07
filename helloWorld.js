38e1d3f2e4ff37d1f266826856f9e1a095edd9c7
Rishi M <81456455+RM13-Pixel@users.noreply.github.com>
2024-05-06 22:48:50 -0400

    Update helloWorld.js

--- helloWorld.js
+++ helloWorld.js
@@ -10,7 +10,7 @@ app.set('trust proxy', 1)
 app.use(cors());
 app.use(express.json({limit: '50mb'}));
 
-let port = 3000;
+let port = 3002;
 
 const server = app.listen(port, ()=> console.log(`Server started on port ${port}`));
 
